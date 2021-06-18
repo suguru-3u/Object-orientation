d39abd6c0484628141bd9b22289887bc78575cc3
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-18 11:52:38 +0900

    Create Table.java

---
+++ babanuki/Table.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Table{
+    private ArrayList disposedCards = new ArrayList();
+
+    // カードを捨てる
+    public void disposedCard(Card card[]){
+
+        for(int index = 0; index < card.length; index ++){
+
+            System.out.println(card[index] + " ");
+        }
+        
+        System.out.println("を捨てました");
+
+        disposedCards.add(crad);
+    }
+}
