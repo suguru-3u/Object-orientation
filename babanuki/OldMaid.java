561d23ce08dd78232593d844d994f6204edaa476
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-19 14:27:43 +0900

    Create OldMaid.java

---
+++ babanuki/OldMaid.java
@@ -0,0 +1,11 @@
+public class OldMaid{
+    public static void main(String[] args){
+
+        // 進行役の生成
+        Master master = new Master();
+
+        // テーブルの生成
+        Table field = new Table();
+        
+    }
+}
