ce079a60d584054f5dd16104a4db966eb6abd7f2
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-19 14:09:55 +0900

    write code in Master.java

---
+++ babanuki/Master.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class Master{
+
+    private ArrayList players = new ArrayList();
+
+    // ゲームの準備を行う
+    public void prepareGame(Hand cards){
+
+        System.out.println("「カードを配ります」");
+
+        crads.shuffle();
+
+        int numberOfCards = cards.getNumberOfCards();
+
+        int numberOfPlayers = players.size();
+
+        for(int index = 0; index < numberOfCards ; index){
+
+            Crad card = cards.pickedCard();
+
+
+            Player player = (Player)players.get(index % numberOfPlayers);
+
+            player.receiveCard(card);
+        }
+    }
+
+    // ゲームを開始する
+    public void startGame(){
+
+        System.out.println("「Gameを開始します」");
+
+        for(int count = 0 ; players.size() > 1 ; count++){
+            
+            int playerIndex = count % players.size();
+            int nextPlayerIndex = (count + 1) % players.size();
+
+            // 指名するプレイヤーの取得
+            Player player = (Player)players.get(playerIndex);
+
+            // 次のプレイヤーの取得
+            Player nextplayer = (Player)players.get(nextPlayerIndex);
+
+            // プレイヤーを指名する
+            System.out.println(player + "さんの番です");
+            player.play(nextPlaye);
+             
+        }
+        System.out.println("「Gameを終了します」");
+    }
+
+    // 上がりを宣言する
+    public void declareWin(Player winner){
+
+        System.out.println(winner + "さんが上がりました");
+
+        // 上がったプレイヤーをリストから外す
+        players.remove(players.indexOf(winner));
+
+        // 残りプレイヤーがひとりになった場合は敗者を表示する
+        if(players.size() == 1){
+            Player loser = (Player)player.get(0);
+            System.out.println(loser + "さんの負けです");
+
+        }
+
+    }
+
+    // ゲームに参加する人を登録する
+    public void registerPlyerer(Player player){
+    
+        players.add(player);
+    
+    }
+
+}
