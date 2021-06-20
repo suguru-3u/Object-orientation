a781c5a700cc96204193e3e9272541a2d51d214f
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-20 11:10:05 +0900

    Create trump.java

---
+++ trump/Card.java
@@ -0,0 +1,11 @@
+package trump;
+
+public class Card{
+    
+    public static final int SUIT_SPADE = 1 ;
+    public static final int SUIT_DIAMONDO = 2 ;
+    public static final int SUIT_SPADE = 3 ;
+    public static final int SUIT_SPADE = 4 ;
+    public static final int SUIT_SPADE = 4 ;
+    public static final int SUIT_SPADE = 13 ;
+}
