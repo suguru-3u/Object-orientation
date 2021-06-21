df6359254836c125b4cce9fbec24e5210f4a3986
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-22 07:50:28 +0900

    Write Main.java

---
+++ study/Main.java
@@ -0,0 +1,35 @@
+public class Main{
+    public static void main(String args[]){
+
+        StringBuilder sb = new StringBuilder();
+        for(int count = 1; count < 101 ; count++){
+            sb.append((String)count);
+            sb.append(",");
+        }
+
+        String[] sbs = sb.split(",");
+    }
+
+    public String fileString(String folder,String file){
+        StringBuilder aaa = new StringBuilder();
+        if(folder.endsWith(String "¥")){
+            aaa.append(folder);
+            aaa.append(file);
+        }else{
+            aaa.append(folder);
+            aaa.append("¥");
+            aaa.append(file);
+        }
+        return aaa;
+    }
+
+    public Date aaa(){
+        Date now = new Date();
+        Calender c = Calender.getInstance();
+        c.getTime(now);
+        int  day = c.get(Calender.DAY_OF_MONTH);
+        c.set(day,day + 100);
+        Date g = c.getTime();
+        SimpleDateFormat f = new SimpleDateFormat("西暦 yyyy年mm月dd日");
+    }
+}
