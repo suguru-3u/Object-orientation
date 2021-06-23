ee93ebd2528fb95d6ef007f9361e4d13f4e83677
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-23 21:11:22 +0900

    Write Code User.java

--- portfolio/User.java
+++ portfolio/User.java
@@ -1,3 +1,18 @@
+/*
+* アプリを利用するユーザークラス
+* ユーザーを抽象化させたHumanクラスを継承している
+* 抽象メソッド[delete]をオーバーライドしないといけない
+*/
+
 public class User extends Human{
-    
+
+    // コンストラクタ
+    public User(String name){
+        super(name);
+    }
+
+    // ユーザーが登録したタスクを削除するメソッド
+    public List<Task> delete(List<Task> task){
+        
+    }
 }
