4d300b48b441434ce20a9f3cd2884886f2bbe0e8
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-23 20:36:38 +0900

    Study Java

--- study/Main.java
+++ study/Main.java
@@ -11,24 +11,49 @@ public class Main{
 
         // String[] sbs = sb.split(",");
 
-        Hero saitou = new Hero("斉藤");
-        Hero suzuki = new Hero("鈴木");
+        // Hero saitou = new Hero("斉藤");
+        // Hero suzuki = new Hero("鈴木");
 
-        ArrayList<Hero> heros = new ArrayList<>(Arrays.asList(saitou,suzuki));
+        // ArrayList<Hero> heros = new ArrayList<>(Arrays.asList(saitou,suzuki));
 
-        heros.stream().forEach(i -> System.out.println(i.getName()));
+        // heros.stream().forEach(i -> System.out.println(i.getName()));
 
-        Map<Hero,Integer> heros2 = new HashMap<Hero,Integer>(){
-            {
-                put(saitou, 3);
-                put(suzuki, 7);
-            }
-        };
+        // Map<Hero,Integer> heros2 = new HashMap<Hero,Integer>(){
+        //     {
+        //         put(saitou, 3);
+        //         put(suzuki, 7);
+        //     }
+        // };
 
-        heros2.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue()).forEach(System.out::println);
+        // heros2.entrySet().stream().map(e -> e.getKey() + ": " + e.getValue()).forEach(System.out::println);
         
         
         // .forEach(i -> System.out.println(i.getKey() ":" i.getValue()));
+
+        // String s = null;
+        // try{
+        //     System.out.println(s.length());
+        // }catch(Exception e){
+        //     System.out.println(e + "をcatchしました");
+        //     // System.out.println(e.printStackTrace());
+        //     System.out.println(e.getMessage());
+        //     System.out.println("s.length()");
+            
+        // }
+
+        // String str = "3plapkonknjkbhb";
+
+        // try{
+        //     int num = Integer.parseInt(str);
+        //     System.out.println(num);
+        // }catch(NumberFormatException e){
+        //     System.out.println(e.getMessage());
+        //     e.printStackTrace();
+        // }
+
+        // System.out.println("Start");
+        // throw new IOException();
+
 
 
 
