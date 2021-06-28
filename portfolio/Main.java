b020bba5e3d49245e0076829fa010e66ffed998c
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-28 13:50:53 +0900

    Study Java

---
+++ portfolio/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+
+        System.out.println("こんにちわ!");
+ 
+        Scanner scan = new Scanner(System.in);
+ 
+        String str = scan.next();
+ 
+        System.out.println("入力された文字は「" + str + "」です");
+    }
+}
