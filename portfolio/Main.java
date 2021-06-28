829dd45e40e19768fdc403d9874243833ebd77a5
Oohira Suguru <suguru.13579515@gmail.com>
2021-06-29 08:40:29 +0900

    Study Java

--- portfolio/Main.java
+++ portfolio/Main.java
@@ -1,12 +1,20 @@
+import java.io.*;
+
 public class Main{
     public static void main(String[] args){
 
-        System.out.println("こんにちわ!");
- 
-        Scanner scan = new Scanner(System.in);
- 
-        String str = scan.next();
- 
-        System.out.println("入力された文字は「" + str + "」です");
+    // System.in -> キーボード入力を受け付けるための引数
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("名前を入力してください：");
+
+    try {
+      // キーボード入力を受け付ける
+      String name = br.readLine();
+
+      System.out.println("こんにちは、" + name + "さん");
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+    
     }
 }
