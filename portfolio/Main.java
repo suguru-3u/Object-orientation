ff18e4d2c07d11d6af15fca46da73f63a177892b
Oohira Suguru <suguru.13579515@gmail.com>
2021-07-08 12:39:53 +0900

    Study Java

--- portfolio/Main.java
+++ portfolio/Main.java
@@ -2,62 +2,11 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import java.sql.*;
 
 
 public class Main{
   public static void main(String[] args){
-
-    Connection con = null;
-    PreparedStatement stmt = null;
-    ResultSet rs = null;
-
-    // SQL文の作成
-    String sql = "SELECT * FROM users";
-
-    try {
-        // JDBCドライバのロード
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        // データベース接続
-        con = DriverManager.getConnection("jdbc:mysql://localhost/java_app", "root", "13579515Ss");
-        // SQL実行準備
-        stmt = con.prepareStatement(sql);
-        // 実行結果取得
-        rs = stmt.executeQuery();
-
-  // データがなくなるまで(rs.next()がfalseになるまで)繰り返す
-        while (rs.next()) {
-            String id = rs.getString("id");
-            String name = rs.getString("name");
-
-            System.out.println(id + ":" + name);
-        }
-    } catch (ClassNotFoundException e) {
-        System.out.println("JDBCドライバのロードでエラーが発生しました");
-    } catch (SQLException e) {
-        System.out.println("データベースへのアクセスでエラーが発生しました。");
-    } finally {
-        try {
-            if (con != null) {
-                con.close();
-            }
-        } catch (SQLException e) {
-            System.out.println("データベースへのアクセスでエラーが発生しました。");
-        }
-    }
-
-
-
-
-
-
-
 
     // インスタンス変数生成
     TaskMemo taskMemo = new TaskMemo();
